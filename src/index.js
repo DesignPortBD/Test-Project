dd20ca6a831287554d8b788b280a6b32cca9d803
Design Port BD <74976993+DesignPortBD@users.noreply.github.com>
2020-11-29 02:09:00 +0600

    Test Project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './router/App';
+import '../src/assets/css/style.css'
+
+ReactDOM.render( <App />
+, document.getElementById('root')
+);
+
